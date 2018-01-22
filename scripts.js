b6f15bc8e82d2b25f41b25cb2ef9eb65354d26ac
Mattbox <mattbox@users.noreply.github.com>
2018-01-22 02:24:50 -0600

    Updating for Textual 7.0.7 compatibility

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@
 /* Theme-wide preferences, as per milky's request */
 var Batonox = {
   fadeNicks: false,           // fade out nicknames when they appear multiple times in a row
-  fadeNicksFreq: 10,          // how frequently to display a nick if they have fadeNickCounts lines in a row
+  fadeNicksFreq: 10,          // how frequently to display a nick if fadeNickCounts lines in a row
   showDateChanges: true,      // show date changes
   squashModes: true,          // if a duplicate mode gets posted to the channel, squash it
   squashTopics: true          // if a duplicate topic gets posted to the channel, squash it
@@ -42,19 +42,19 @@ var NickColorGenerator = (function () {
     var i, inlineNicks, nick;
 
     // Start alternative nick colouring procedure
-    var selectNick = message.querySelector('.sender:not([mtype=myself])');
+    var selectNick = message.querySelector('.sender:not([data-member-type=myself])');
     if (selectNick == null){
       return;
     }
-    selectNick.removeAttribute('colornumber');
+    // selectNick.removeAttribute('colornumber');
 
     inlineNicks = message.querySelectorAll('.inline_nickname');
 
-    this.generateColorFromNickname(selectNick.getAttribute('nickname'),
+    this.generateColorFromNickname(selectNick.dataset.nickname,
       function(nickcolor) {
         selectNick.style.color = nickcolor;
 
-        if (message.getAttribute('ltype') === 'action') {
+        if (message.dataset.lineType === 'action') {
           message.querySelector('.message').style.color = nickcolor;
         }
       }
@@ -62,10 +62,9 @@ var NickColorGenerator = (function () {
 
     var self = this;
     for (i = 0; i < inlineNicks.length; i++) {
-      inlineNicks[i].removeAttribute('colornumber');
       nick = inlineNicks[i].textContent;
-      if (inlineNicks[i].getAttribute('mode').length > 0) {
-        nick = nick.replace(inlineNicks[i].getAttribute('mode'), '');
+      if (inlineNicks[i].dataset.mode.length > 0) {
+        nick = nick.replace(inlineNicks[i].dataset.mode, '');
       }
       var inlineNick = inlineNicks[i];
       (function(inlineNickname) {
@@ -146,7 +145,7 @@ function dateChange(e) {
     return;
   }
 
-  timestamp = parseFloat(e.getAttribute('timestamp')) * 1000;
+  timestamp = parseFloat(e.dataset.timestamp) * 1000;
   datetime = new Date(timestamp);
 
   year = datetime.getFullYear();
@@ -157,7 +156,7 @@ function dateChange(e) {
   // Occasionally when replaying, Textual will post messages in the future, and then jump backwards
   // As such, we'll ignore all joins, modes, and topics, if they're more than MAXTIMEOFFSET milliseconds
   // from the current time
-  ltype = e.getAttribute('ltype');
+  ltype = e.dataset.lineType;
   if (ltype !== 'privmsg') {
     if (Date.now() - timestamp > MAXTIMEOFFSET) {
       return;
@@ -239,15 +238,15 @@ Textual.newMessagePostedToView = function (line) {
   var clone, elem, getEmbeddedImages, i, mode, messageText, sender, topic;
 
   // reset the message count and previous nick, when you rejoin a channel
-  if (message.getAttribute('ltype') !== 'privmsg') {
+  if (message.dataset.lineType !== 'privmsg') {
     rs.nick.count = 1;
     rs.nick.nick = undefined;
   }
 
   // if it's a private message, colorize the nick and then track the state and fade away the nicks if needed
-  if (message.getAttribute('ltype') === 'privmsg' || message.getAttribute('ltype') === 'action') {
+  if (message.dataset.lineType === 'privmsg' || message.dataset.lineType  === 'action') {
     sender = message.getElementsByClassName('sender')[0];
-    if (sender.getAttribute('coloroverride') !== 'true') {
+    if (sender.dataset.overrideColor !== 'true') {
       new NickColorGenerator(message); // colorized the nick
     }
 
@@ -267,37 +266,14 @@ Textual.newMessagePostedToView = function (line) {
       rs.nick.count  = 1;
       rs.nick.delete = false;
     }
-
-    // Track the previous message's id
-    rs.nick.id = message.getAttribute('id');
-
-    // Copy the message into the hidden history
-    clone = message.cloneNode(true);
-    clone.removeAttribute('id');
-    rs.history.appendChild(clone);
-
-    // Colorize it as well
-    if (sender.getAttribute('coloroverride') !== 'true') {
-      new NickColorGenerator(clone); // colorized the nick
-    }
-
-    // Remove old messages, if the history is longer than three messages
-    if (rs.history.childElementCount > 2) {
-      rs.history.removeChild(rs.history.childNodes[0]);
-
-      // Hide the first nick in the hidden history, if it's the same as the second
-      if ((rs.nick.count > 1) && (message.getAttribute('ltype') !== 'action')) {
-        rs.history.getElementsByClassName('sender')[0].style.visibility = 'hidden';
-      }
-    }
   }
 
   /* Let's kill topics that appear where they had already been set before
      This happens when you join a room (like a reconnect) that you had been in and seen the topic before */
-  if (Batonox.squashTopics === true && message.getAttribute('ltype') === 'topic') {
+  if (Batonox.squashTopics === true && message.dataset.lineType === 'topic') {
     topic = message.getElementsByClassName('message')[0].textContent.replace('Topic is ', '').replace(/\s+/, '');
 
-    if (message.getAttribute('command') === '332') { // an actual topic change
+    if (message.dataset.command === '332') { // an actual topic change
       // hide the topic if it's the same topic again
       if (topic === rs.topic.topic) {
         message.parentNode.removeChild(message);
@@ -307,14 +283,14 @@ Textual.newMessagePostedToView = function (line) {
       rs.topic.topic = topic;
     }
 
-    if ((message.getAttribute('command') === '333') && (rs.topic.delete === true)) {
+    if ((message.dataset.command === '333') && (rs.topic.delete === true)) {
       message.parentNode.removeChild(message);
       rs.topic.delete = false;
     }
   }
 
   // much like we suppress duplicate topics, we want to suppress duplicate modes
-  if (Batonox.squashModes === true && message.getAttribute('ltype') === 'mode') {
+  if (Batonox.squashModes === true && message.dataset.lineType === 'mode') {
     mode = message.getElementsByClassName('message')[0].textContent.replace(/\s+/, '');
 
     if (mode === rs.mode.mode) {
@@ -325,7 +301,7 @@ Textual.newMessagePostedToView = function (line) {
   }
 
   // hide messages about yourself joining
-  if ((message.getAttribute('ltype') === 'join') || (message.getAttribute('ltype') === 'part')) {
+  if ((message.dataset.lineType === 'join') || (message.dataset.lineType === 'part')) {
     app.localUserNickname(
       function(returnValue) {
         if (returnValue == message.getElementsByClassName('message')[0].getElementsByTagName('b')[0].textContent) {
@@ -337,7 +313,7 @@ Textual.newMessagePostedToView = function (line) {
 
   /* clear out all the old disconnect messages, if you're currently connected to the channel
      note that normally Textual.handleEvent will catch this, but if you reload a theme, they will reappear */
-  if ((message.getAttribute('ltype') === 'debug') && (message.getAttribute('command') === '-100')) {
+  if ((message.dataset.lineType === 'debug') && (message.dataset.command === '-100')) {
     app.channelIsJoined(
       function(returnValue) {
         if (returnValue && message.getElementsByClassName('message')[0].textContent.search('Disconnect') !== -1) {
@@ -347,15 +323,15 @@ Textual.newMessagePostedToView = function (line) {
     );
   } else {
     // call the dateChange() function, for any message with a timestamp that's not a debug message
-    if (message.getAttribute('timestamp')) {
+    if (message.dataset.timestamp) {
       dateChange(message);
     }
   }
 
-  if (message.getAttribute('encrypted') === 'true') {
+  if (message.dataset.encrypted === 'true') {
     messageText = message.querySelector('.innerMessage');
     if (messageText.innerText.indexOf('+OK') !== -1) {
-      message.setAttribute('encrypted', 'failed');
+      message.dataset.encrypted = 'failed';
     }
   }
 
@@ -383,42 +359,29 @@ Textual.viewBodyDidLoad = function () {
   'use strict';
   Textual.fadeOutLoadingScreen(1.00, 0.95);
 
-  /* Disable date changes on OS X Mountain Lion because WebKit does not have some of
-     the features that this feature depends on (e.g. -webkit-flex) */
-  if (document.documentElement.getAttribute("systemversion").indexOf("10.8.") === 0) {
-    Batonox.showDateChanges = false;
+  Textual.viewFinishedLoadingHistory = function () {
+    'use strict';
+
+    // enable the history view, but only a bit after this gets called
+    setTimeout(function() {
+      rs.enableHistoryView = true;
+    }, 850);
   }
-};
-
-Textual.viewInitiated = function () {
-  'use strict';
-
   /* When the view is loaded, create a hidden history div which we display if there is scrollback */
-  var body = document.getElementById('body_home'), div = document.createElement('div');
+  var body = document.getElementById('body'), div = document.createElement('div');
   div.id = 'scrolling_history';
   document.getElementsByTagName('body')[0].appendChild(div);
   rs.history = div;
 
   /* setup the scrolling event to display the hidden history if the bottom element isn't in the viewport
-     also hide the topic bar when scrolling */
-  window.onscroll = function () {
-    var line, lines;
-    var topic = document.getElementById('topic_bar');
-
-    lines = body.getElementsByClassName('line');
-    if (lines.length < 2) {
+     also hide the topic bar when scrolling.  Note that we have to set a timer here so that the history
+     div doesn't appear in the viewport on normal inserts, which cause scroll effects */
+  window.addEventListener('scroll', function () {
+    // check to see if a bit of time has passed since we loaded the history
+    if (!rs.enableHistoryView) {
       return;
     }
-    line = lines[lines.length - 1];
 
-    if (isMessageInViewport(line) === false) {
-      // scrollback
-      rs.history.style.display = 'inline';
-      if (topic) { topic.style.visibility = 'hidden'; }
-    } else {
-      // at the bottom
-      rs.history.style.display = 'none';
-      if (topic) { topic.style.visibility = 'visible'; }
-    }
-  };
+    rs.scrollTimer = setTimeout(toggleHistoryIfScrolled, 100);
+  });
 };
